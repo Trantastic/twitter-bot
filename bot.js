66abf786f76588e1361d4e42a87b14a30d33be85
Yvette <yvettetran@gmail.com>
2018-08-28 21:44:58 -0700

    bug, data is undefined

--- bot.js
+++ bot.js
@@ -5,16 +5,22 @@ var config = require("./config");
 var T = new Twit(config);
 
 
-var keyword = readlineSync.question("What keyword do you want to use?");
-var count = readlineSync.question("How many do you want displayed?");
+var keyword = readlineSync.question("What keyword do you want to use? ");
+var count = readlineSync.question("How many do you want displayed? ");
 var params = { q: keyword, count: parseInt(count)};
 
-T.get('search/tweets', params, getTweets);
+console.log(params);
 
-function getTweets() {
-	var tweets = data.statuses;
+// T.get('search/tweets', params, getTweets);
 
-	for(var i = 0; i < tweets.length; i++) {
-		console.log(tweets[i].text);
-	}
-}
+// function getTweets() {
+// 	var tweets = data.statuses[0];
+
+// 	for(var i = 0; i < tweets.length; i++) {
+// 		console.log(tweets[i].text);
+// 	}
+// }
+
+// T.get('search/tweets', { q: "california", count: 1 }, function(err, data, res) {
+// 	console.log(data.statuses[0].text);
+// });
