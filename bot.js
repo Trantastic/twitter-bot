8bf5ba9547bc6dae5fe891d6131ba36a88d2d00e
Yvette <yvettetran@gmail.com>
2018-08-28 20:58:17 -0700

    added readlineSync npm

--- bot.js
+++ bot.js
@@ -1,4 +1,20 @@
+var readlineSync = require("readline-sync")
 var Twit = require("twit");
-var congif = require("./config");
+var config = require("./config");
  
 var T = new Twit(config);
+
+
+var keyword = readlineSync.question("What keyword do you want to use?");
+var count = readlineSync.question("How many do you want displayed?");
+var params = { q: keyword, count: parseInt(count)};
+
+T.get('search/tweets', params, getTweets);
+
+function getTweets() {
+	var tweets = data.statuses;
+
+	for(var i = 0; i < tweets.length; i++) {
+		console.log(tweets[i].text);
+	}
+}
